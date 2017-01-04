b71a267ec980642a0318e944582f60dc21ed26c8
ZeeLog <Danjel.Zammit@gmail.com>
2017-01-04 16:55:12 +0100

    pollux_windy: Rebrand to LineageOS
    Change-Id: Ia919687813b90ccecf311df391b75d6f112d39a0

--- lineage.mk
+++ lineage.mk
@@ -2,8 +2,8 @@ $(call inherit-product, device/sony/pollux_windy/full_pollux_windy.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGP311 BUILD_FINGERPRINT=Sony/SGP311/SGP311:5.1.1/10.7.A.0.222/900225891:user/release-keys PRIVATE_BUILD_DESC="SGP311-user 5.1.1 10.7.A.0.222 900225891 test-keys"
 
-# Inherit some common CM stuff.
+# Inherit some common stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
-PRODUCT_NAME := cm_pollux_windy
+PRODUCT_NAME := lineage_pollux_windy
 PRODUCT_DEVICE := pollux_windy
